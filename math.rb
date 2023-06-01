ed3617de21cd4d6c96919a1df17ab4c8fb65122a
Kairos <hestiatay@gmail.com>
2023-06-01 14:59:52 +0800

    Create math.rb

---
+++ math.rb
@@ -0,0 +1,15 @@
+# Addition
+result = 3 + 4
+puts "Addition: #{result}"
+
+# Subtraction
+result = 7 - 2
+puts "Subtraction: #{result}"
+
+# Multiplication
+result = 5 * 6
+puts "Multiplication: #{result}"
+
+# Division
+result = 10 / 2
+puts "Division: #{result}"
