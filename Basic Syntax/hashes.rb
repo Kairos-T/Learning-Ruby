cfa20d9d07ddd326add3b090145bfd02a5732483
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-07-08 22:51:28 +0800

    Create hashes.rb

---
+++ Basic Syntax/hashes.rb
@@ -0,0 +1,11 @@
+# like dictionaries in python
+
+student = {
+  "name" => "Kairos",
+  "age" => 17,
+  "school" => "Ngee Ann Polytechnic"
+}
+
+puts student["name"]
+puts student["age"]
+puts student["school"]
