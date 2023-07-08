6fe56b565a8e37efeb9c2e731669cdbe9207c4a6
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-07-08 22:51:21 +0800

    Create loops.rb

---
+++ Basic Syntax/loops.rb
@@ -0,0 +1,22 @@
+# while loop
+i = 0
+while i<5
+  puts i
+  i += 1
+end
+
+# for loop
+for i in 0..5
+  puts i
+end
+
+# each loop (for arrays)
+(0..4) each do |i|
+  puts i
+end
+#output:
+# 0
+# 1
+# 2
+# 3
+# 4
